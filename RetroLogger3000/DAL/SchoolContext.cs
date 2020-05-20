721c225f57b9c2359cb4323ad9460f0df2e9ee61
Alex <kylecox213@gmail.com>
2020-05-19 20:57:54 -0400

    Add project files.

---
+++ RetroLogger3000/DAL/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using RetroLogger3000.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace RetroLogger3000.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ RetroLogger3000/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RetroLogger3000.MvcApplication" Language="C#" %>
