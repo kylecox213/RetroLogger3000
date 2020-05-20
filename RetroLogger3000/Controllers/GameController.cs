c3eee15ca4fb161e475217412ad065d63bb704d7
Alex <kylecox213@gmail.com>
2020-05-20 02:37:23 -0400

    updated HttpPost edit method

--- RetroLogger3000/Controllers/GameController.cs
+++ RetroLogger3000/Controllers/GameController.cs
@@ -58,11 +58,11 @@ namespace RetroLogger3000.Controllers
                     return RedirectToAction("Index");
                 }
             }
-            catch (DataException /* dex */)
-            {
-                //Log the error (uncomment dex variable name and add a line here to write a log.
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            }
+                catch (DataException /* dex */)
+    {
+        //Log the error (uncomment dex variable name and add a line here to write a log.
+        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+    }
 
             return View(game);
         }
@@ -85,17 +85,31 @@ namespace RetroLogger3000.Controllers
         // POST: Student/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,Year,Rank,Clean,Complete,Beaten,Duplicate")] Game game)
+        public ActionResult EditPost(int? id)
         {
-            if (ModelState.IsValid)
+            if (id == null)
             {
-                db.Entry(game).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            return View(game);
+            var gameToUpdate = db.Games.Find(id);
+            if (TryUpdateModel(gameToUpdate, "",
+               new string[] { "ID", "Title", "Year", "Rank", "Clean", "Complete", "Beaten", "Duplicate" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DataException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            return View(gameToUpdate);
         }
 
         // GET: Student/Delete/5
