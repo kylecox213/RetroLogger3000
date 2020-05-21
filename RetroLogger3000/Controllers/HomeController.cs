33d189b115cd375931c62cad1e236496ccb03731
Alex <kylecox213@gmail.com>
2020-05-21 16:34:57 -0400

    updated About page; created the View Model; modified the Home Controller; modified the About View

--- RetroLogger3000/Controllers/HomeController.cs
+++ RetroLogger3000/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RetroLogger3000.DAL;
+using RetroLogger3000.Models;
+using RetroLogger3000.ViewModels;
 
 namespace RetroLogger3000.Controllers
 {
 	public class HomeController : Controller
 	{
+		private RetroContext db = new RetroContext();
 		public ActionResult Index()
 		{
 			return View();
@@ -15,9 +19,20 @@ namespace RetroLogger3000.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			IQueryable<GamesTotal> data = from game in db.Games
+					   group game by game.Title into titleGroup
+					   select new GamesTotal()
+					   {
+							Title = titleGroup.Key,
+							GameCount = titleGroup.Count()
+					   };
+			return View(data.ToList());
+		}
 
-			return View();
+		protected override void Dispose(bool disposing)
+		{
+			db.Dispose();
+			base.Dispose(disposing);
 		}
 
 		public ActionResult Contact()
---
+++ RetroLogger3000/ViewModels/GamesTotal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetroLogger3000.ViewModels
+{
+	public class GamesTotal
+	{
+		public string Title { get; set; }
+
+		public int GameCount { get; set; }
+	}
+}
