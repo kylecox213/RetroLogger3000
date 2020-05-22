55fc2fcefd7a1b807bd0f363d78c61c90edcf0c1
Alex <kylecox213@gmail.com>
2020-05-21 23:38:04 -0400

    added logging classes to the global.asax application startup

--- RetroLogger3000/Global.asax.cs
+++ RetroLogger3000/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using RetroLogger3000.DAL;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace RetroLogger3000
 {
@@ -16,6 +18,8 @@ namespace RetroLogger3000
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			DbInterception.Add(new GameInterceptorTransientErrors());
+			DbInterception.Add(new GameInterceptorLogging());
 		}
 	}
 }
