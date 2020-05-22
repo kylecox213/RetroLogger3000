390967fab79258afa776e2d7cc68fd4a658aae7d
Alex <kylecox213@gmail.com>
2020-05-21 23:59:48 -0400

    enabling code first migrations

---
+++ RetroLogger3000/Migrations/202005220349496_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿namespace RetroLogger3000.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.GameConsole",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Type = c.String(),
+                        Model = c.String(),
+                        Functional = c.Boolean(nullable: false),
+                        GamePadID = c.Int(nullable: false),
+                        GameID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Game", t => t.GameID, cascadeDelete: true)
+                .ForeignKey("dbo.GamePad", t => t.GamePadID, cascadeDelete: true)
+                .Index(t => t.GamePadID)
+                .Index(t => t.GameID);
+            
+            CreateTable(
+                "dbo.Game",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Year = c.Int(nullable: false),
+                        Rank = c.Int(nullable: false),
+                        Clean = c.Boolean(nullable: false),
+                        Complete = c.Boolean(nullable: false),
+                        Beaten = c.Boolean(nullable: false),
+                        Duplicate = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.GamePad",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Model = c.String(),
+                        Type = c.String(),
+                        Functional = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.GameConsole", "GamePadID", "dbo.GamePad");
+            DropForeignKey("dbo.GameConsole", "GameID", "dbo.Game");
+            DropIndex("dbo.GameConsole", new[] { "GameID" });
+            DropIndex("dbo.GameConsole", new[] { "GamePadID" });
+            DropTable("dbo.GamePad");
+            DropTable("dbo.Game");
+            DropTable("dbo.GameConsole");
+        }
+    }
+}
---
+++ RetroLogger3000/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+﻿namespace RetroLogger3000.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RetroLogger3000.DAL.RetroContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RetroLogger3000.DAL.RetroContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            //
+            //    context.RetroGame.AddOrUpdate(
+            //       r => r.Title,
+            //       new RetroGame { Title = "Robocop" },
+            //       new RetroGame { Title = "Air Fortress" },
+            //       new RetroGame { Title = "Dig Dug II" }
+            //    );
+        }
+    }
+}
