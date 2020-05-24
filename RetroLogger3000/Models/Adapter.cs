770056dc904be3531f610a7afe35e8c63d5f8244
Alex <kylecox213@gmail.com>
2020-05-24 03:08:50 -0400

    Created adapter class

---
+++ RetroLogger3000/Models/Adapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RetroLogger3000.Models
+{
+	public class Adapter
+	{
+		public int ID { get; set; }
+
+		[Required]
+		[StringLength(25)]
+		public string Type { get; set; }
+		public int Length { get; set; }
+		public bool Functional { get; set; }
+
+		public virtual GameConsole GameConsole { get; set; }
+	}
+}
